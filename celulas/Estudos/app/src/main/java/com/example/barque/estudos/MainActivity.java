f75d4e97e4d1e8728a783eb3a5d64e067b4bdacd
lucasbarque <lucasbarquedasilva@hotmail.com>
2016-12-17 14:15:19 -0300

    teste

---
+++ celulas/Estudos/app/src/main/java/com/example/barque/estudos/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.barque.estudos;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.barque.estudos.util.Util;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Olá Lucas Barque");
+
+
+        Util.showMessageToast(this, "Toast Rodando neguinho!");
+
+        Util.showMsgAlertOK(this, "Lucas Barque", "Essa é a mensagem");
+
+
+
+    }
+}
